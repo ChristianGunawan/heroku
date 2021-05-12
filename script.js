0836679c2867b8402641a4ee725df04cd7a20df3
Christian Gunawan <57822261+ChristianGunawan@users.noreply.github.com>
2021-05-12 18:22:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req,res){
-    res.setEncoding("WORKING");
-});
-
 app.listen(process.env.PORT || 5000);
 
 kataPenting = ['kuis', 'ujian', 'tucil', 'tubes', 'praktikum'];
